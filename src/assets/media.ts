0a2472bde1ec1ad88c3607d29af0699689398c90
Rony Antolinez <rony1710.dev@gmail.com>
2024-11-06 18:39:06 -0500

    feat: add icons and icon component

--- src/assets/media.ts
+++ src/assets/media.ts
@@ -1,10 +1,17 @@
-import SearchIcon from './icons/searchIcon.svg'
+// Icons
+import SearchIcon from './icons/Search.svg'
+import StartIcon from './icons/Star.svg'
+import ChieldIcon from './icons/Chield_alt.svg'
+import NestingIcon from './icons/Nesting.svg'
 
 // images
 import GithubLogo from './images/github-mark-white.png'
 
 export const Icons = {
-    searchIcon: SearchIcon
+    searchIcon: SearchIcon,
+    startIcon: StartIcon,
+    chieldIcon: ChieldIcon,
+    nestingIcon: NestingIcon,
 }
 
 export const Images = {
